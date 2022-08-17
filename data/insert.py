4748355958c376f04f1e675450527c17e16a73a7
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-08-18 04:14:36 +0900

    fix: Artist 데이터에 id (pk) 추가 zip()을 이용한 여러 리스트 중첩 for문에서 마지막 아티스트가 계속 누락되는 이슈가 있었음. zip()을 사용해 여러 리스트를 중첩하여 for문을 사용할 때, 리스트들의 길이가 서로 다르면 제일 짧은 것을 기준으로 함 알고 보니 이미지 파일이 하나 누락되어 있어 발생한 에러

---
+++ data/insert.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import json
+from collections import OrderedDict
+import glob
+
+ARTIST_ID={}
+# {'소녀시대': 1, '레드벨벳': 2, '에스파': 3, 'BTS': 4, '세븐틴': 5, '블랙핑크': 6, 
+# '트와이스': 7, 'ITZY': 8, '아이브': 9, '몬스타액스': 10, '아이들': 11, '아이유': 12}
+
+def makeJsonData_Artist():
+  artist_txt = open('artist.txt', 'r')
+  artist_csv = pd.read_csv(artist_txt, sep='\t')
+  artist_data_list=[]
+
+  id_list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  artist_list=artist_csv['artist']
+  agency_list=artist_csv['agency']
+  image_list=glob.glob('./img/artist/*.jpg') # 경로 확인 필요
+
+  for id, artist, agency, img in zip(id_list, artist_list, agency_list, image_list):
+    artist_data=OrderedDict()
+    artist_data["model"]="album.Artist"
+    artist_data["fields"]={
+      'id' : id,
+      'artist_name' : artist,
+      'agency' : agency,
+      'artist_image' : img,
+    }
+    ARTIST_ID[artist]=id
+    artist_data_list.append(artist_data)
+  
+  with open('artist-data.json', 'w', encoding="utf-8") as make_file:
+    json.dump(artist_data_list, make_file, ensure_ascii=False, indent="\t")
+
+
+def makeJsonData_Album():
+  album_xlsx=pd.read_excel('album.xlsx')
+  print(album_xlsx)
+  album_data_list=[]
+  
+  album_name_list=album_xlsx['앨범명']
+  agency_list=album_xlsx['소속사']
+  created_year_list=album_xlsx['발매년도']
+  created_month_list=album_xlsx['발매월']
+  artist_list=album_xlsx['아티스트']
+  music_list=album_xlsx['수록곡목록']
+  price_withT_list=album_xlsx['응모권포함가격']
+  price_withoutT_list=album_xlsx['응모권미포함가격']
+  album_type_list=album_xlsx['앨범 종류']
+
+  for album, agency, artist, albumType, year, month, music, priceWithT, priceWithout in zip(album_name_list, agency_list, artist_list, album_type_list, 
+  created_year_list, created_month_list, music_list, price_withT_list, price_withoutT_list):
+    album_data=OrderedDict()
+    album_data["model"]="album.Album"
+    album_data["fields"]={
+      'name' : album,
+      'agency' : agency,
+      'created_at' : 'year',
+      'artist' : ARTIST_ID[artist],
+      'album_image' : '',
+      'music_list' : music,
+      'price_with_ticket' : priceWithT,
+      'price_without_ticket' : priceWithout,
+    }
+    album_data_list.append(album_data)
+  
+  with open('album-data.json', 'w', encoding="utf-8") as make_file:
+    json.dump(album_data_list, make_file, ensure_ascii=False, indent="\t")
+
+
+
+
+
+# makeJsonData 함수 실행
+makeJsonData_Artist()
+makeJsonData_Album()
