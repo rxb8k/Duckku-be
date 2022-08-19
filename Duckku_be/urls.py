3a4c19406747fb52c14472a9f4d0668510e901c3
jschan0911@naver.com <jschan0911@naver.com>
2022-08-19 16:56:50 +0900

    feat:edit_album

--- Duckku_be/urls.py
+++ Duckku_be/urls.py
@@ -32,20 +32,21 @@ urlpatterns = [
     path('my_artist_list/<int:user_id>', artist_views.my_artist_list.as_view()),    #승찬
     path('my_artist_list/delete/<int:artist_id>/<int:user_id>', artist_views.delete_my),    #승찬
     path('show_album_info/<int:sang_album_id>', album_views.AlbumInfo.as_view()),
-    path('buy_albums/<int:sang_album_id>', album_views.BuyAlbum.as_view()),
-    path('show_subalbum_list', album_views.ShowSubAlbumList.as_view()),
-    path('add_subalbum/<int:sang_album_id>', album_views.AddSubAlbum.as_view()),
-    path('my_artist_list/show_album_list', album_views.Show_my_artist_AlbumList.as_view()),
-    path('my_artist_list/show_album_list/sort_popular', album_views.Show_artist_list_album_list_sort_popular.as_view()),
-    path('my_artist_list/show_album_list/sort_created_at', album_views.Show_artist_list_album_list_sort_created_at.as_view()),
+    path('buy_albums/<int:sang_album_id>/<int:user_id>', album_views.BuyAlbum.as_view()),
+    path('show_subalbum_list/<int:user_id>', album_views.ShowSubAlbumList.as_view()),
+    path('add_subalbum/<int:sang_album_id>/<int:user_id>', album_views.AddSubAlbum.as_view()),
+    path('my_artist_list/show_album_list/<int:user_id>', album_views.Show_my_artist_AlbumList.as_view()),
+    path('my_artist_list/show_album_list/sort_popular/<int:user_id>', album_views.Show_artist_list_album_list_sort_popular.as_view()),
+    path('my_artist_list/show_album_list/sort_created_at/<int:user_id>', album_views.Show_artist_list_album_list_sort_created_at.as_view()),
     #path('ShowSubAlbumList', album_views.Show_my_artist_AlbumList),
-    path("<int:artist_id>/buy_album_list", album_views.buy_album_list.as_view()),
-    path("<int:artist_id>/buy_photo_card_list", album_views.buy_photo_card_list.as_view()),
-    path('<int:artist_id>/buy_ticket_count', album_views.buy_ticket_count.as_view()),
-    path('<int:artist_id>/ticket_use_complete', album_views.ticket_use_complete.as_view()),
+    path("<int:artist_id>/buy_album_list/<int:user_id>", album_views.buy_album_list.as_view()),
+    path("<int:artist_id>/buy_photo_card_list/<int:user_id>", album_views.buy_photo_card_list.as_view()),
+    path('<int:artist_id>/buy_ticket_count/<int:user_id>', album_views.buy_ticket_count.as_view()),
+    path('<int:artist_id>/ticket_use_complete/<int:user_id>', album_views.ticket_use_complete.as_view()),
     path('qr/<int:photocard_id>', album_views.about_photocard_pr.as_view()),
     path('album_music_list_info/<int:sang_album_id>', album_views.Album_music_list_info.as_view()),
     path('show_all_artist_info', album_views.show_all_artist_info.as_view()),
+    #path('send_email', album_views.send_email.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
